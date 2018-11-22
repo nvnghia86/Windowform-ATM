ab1b12b96875ab6fb7980eff8ffa086db3c20647
TortoiseGit <nghiakho97@gmail.com>
2018-11-22 23:57:15 +0700

    a

--- ATM/ATM/ATM/UC/UC2/Form1.cs
+++ ATM/ATM/ATM/UC/UC2/Form1.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BUL;
+using DAL;
 
-namespace ATM
+
+namespace ATM.UC.UC2
 {
-    public partial class HoaDon : UserControl
+    public partial class Form1 : Form
     {
-        
-        public HoaDon()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void HoaDon_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            string sql = "select ID from ATM";
             DateTime aDateTime = DateTime.Now;
             lbNgayGD.Text = aDateTime.ToString();
-
+            lbID.Text = sql.ToString();
+            lbPhiGD.Text = "1100.00 VNĐ";
         }
-        
     }
 }
